3ee2637d96c3ca654904322a25380477d2d4f31f
jps777 <jaipalprachothan@gmail.com>
2022-06-10 11:21:29 +0530

    third commit

--- src/main/java/com/cicd/SimpleJava.java
+++ src/main/java/com/cicd/SimpleJava.java
@@ -5,7 +5,7 @@ public class SimpleJava {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("working good world commit 2");
+		System.out.println("working good world commit 3");
 
 	}
 
