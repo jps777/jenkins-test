bf2af79f9d3d2bbb9464a04be223e8916dba579d
jps777 <jaipalprachothan@gmail.com>
2022-06-10 10:56:37 +0530

    first commit

---
+++ src/main/java/com/cicd/SimpleJava.java
@@ -0,0 +1,12 @@
+package com.cicd;
+
+public class SimpleJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("working good world");
+
+	}
+
+}
